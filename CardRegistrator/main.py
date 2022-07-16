7458c45ab398e00b2922f43830c4b1e1e5f77492
Firdaus Nuur Rhamadhan <firdausnuurr@gmail.com>
2022-07-17 03:43:44 +0700

    error update

--- CardRegistrator/main.py
+++ CardRegistrator/main.py
@@ -1,54 +1,28 @@
+import eel
 import os
 import eel
+from random import randint
 import rfidReader as rfid
-import mysqlConnection as conn
-import multiprocessing
 
-guiPath = os.path.dirname(os.path.realpath(__file__)) + '/GUI'
-eel.init(guiPath)
+@eel.expose
+def hello():
+    print("Hello from Python")
+
+print('Starting CardRegistrator')
+eel.init(f'{os.path.dirname(os.path.realpath(__file__))}/GUI')
+eel.start("main.html") 
+print('Starting GUI')
 eel.start("main.html", mode='edge',block=False)
 
-dataPath=f'{os.path.dirname(os.path.realpath(__file__))}/data'
-
-def updateData(dataaa):
-    with open(dataPath+'/card.csv', 'w') as f:
-        f.writelines(f'{dataaa}')
-def readData():
-    with open(dataPath+'/card.csv', 'r') as f:
-        data = f.readline()
-        return data
-        
-def readRFIDandWritetoCSV():
+def main():
+    print('Starting main')
     while True:
-        eel.sleep(1)
-        data = rfid.rfid_reader()
-        print('UID : '+data)
-        updateData(data)
-def readCSVandWritetoDB():
-    while True:
-        data = readData()
-        conn.data_checker(data)
-        
+        eel.sleep(0.01)
+        # data = rfid.rfid_reader() # read RFID tag
+        data = '123456789' # DUMY read RFID tag
+        print(data)
+        eel.rfidData(data)
 
-
-if __name__ == "__main__":
-    # creating processes
-    p1 = multiprocessing.Process(target=readRFIDandWritetoCSV, args=())
-    p2 = multiprocessing.Process(target=readCSVandWritetoDB, args=())
-
-    # starting process 1
-    p1.start()
-    # starting process 2
-    p2.start()
-
-    # wait until process 1 is finished
-    p1.join()
-    # wait until process 2 is finished
-    p2.join()
-
-    # both processes finished
-    print("Done!")
-        
-# print("Main loop started")
-# main()
-# print("Main loop ended")
+print("Main loop started")
+main()
+print("Main loop ended") 
--- CardRegistrator/rfidReader.py
+++ CardRegistrator/rfidReader.py
@@ -1,22 +1,9 @@
 import serial
 import time
-import os
 
-arduino = serial.Serial(port='COM7', baudrate=115200, timeout=.1)
-path=f'{os.path.dirname(os.path.realpath(__file__))}/data'
-
-def updateData(dataaa):
-    with open(path+'/card.csv', 'w') as f:
-        f.writelines(f'{dataaa}')
+arduino = serial.Serial(port='COM7', baudrate=9600, timeout=.1)
 
 def rfid_reader():
     time.sleep(0.05)
     data = arduino.readline().decode("utf-8")
-    updateData(data)
-    return data
-
-def main():
-    while True:
-        print(rfid_reader())
-        
-main()
+    return data 
